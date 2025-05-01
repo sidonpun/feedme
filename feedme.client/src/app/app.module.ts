e1768d01342b2be38b4523ec2e937e883abe7e28
maksim <Moonl3rr@gmail.com>
2025-05-01 19:19:28 +0400

    123

--- feedme.client/src/app/app.module.ts
+++ feedme.client/src/app/app.module.ts
@@ -1,46 +1,48 @@
+// src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ContentComponent } from './components/content/content.component';
-import { CatalogComponent } from './components/catalog/catalog.component';
-import { PopupComponent } from './components/popup/popup.component';
-import { InfoCardsComponent } from './components/info-cards/info-cards.component';
-import { InfoCardsWrapperComponent } from './components/info-cards-wrapper/info-cards-wrapper.component';
-import { WarehouseTabsComponent } from './components/warehouse-tabs/warehouse-tabs.component';
-import { WarehouseTableComponent } from './components/warehouse-table/warehouse-table.component';
-import { HistoryPopupComponent } from './components/history-popup/history-popup.component';
-import { SupplyControlsComponent } from './components/supply-controls/supply-controls.component';
-import { EditStockPopupComponent } from './components/edit-stock-popup/edit-stock-popup.component';
-import { NewProductComponent } from './components/new-product/new-product.component';
 import { TableControlsComponent } from './components/table-controls/table-controls.component';
 import { WarehouseComponent } from './components/warehouse/warehouse.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
+import { EditStockPopupComponent } from './components/edit-stock-popup/edit-stock-popup.component';
+import { InfoCardsWrapperComponent } from './components/info-cards-wrapper/info-cards-wrapper.component';
+
+const routes: Routes = [
+  // … ваши маршруты
+];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot([]),
+  declarations: [
     AppComponent,
     SidebarComponent,
     HeaderComponent,
     ContentComponent,
-    CatalogComponent,
-    PopupComponent,
-    InfoCardsComponent,
-    InfoCardsWrapperComponent,
-    WarehouseTabsComponent,
-    WarehouseTableComponent,
-    HistoryPopupComponent,
-    SupplyControlsComponent,
-    EditStockPopupComponent,
-    NewProductComponent,
     TableControlsComponent,
-    WarehouseComponent
+    WarehouseComponent,
+    NewProductComponent,
+    EditStockPopupComponent,
+    InfoCardsWrapperComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   bootstrap: [AppComponent]
 })
