e9baba18e96f522ca8de8dcd2dbb0d49122d58a2
sidonpun <sidonypunjek@hotmail.com>
2025-06-11 23:23:14 +0400

    1
    1

---
+++ feedme.Server/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+namespace feedme.Server;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.AddServiceDefaults();
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+            .AddNegotiate();
+
+        builder.Services.AddAuthorization(options =>
+        {
+            // By default, all incoming requests will be authorized according to the default policy.
+            options.FallbackPolicy = options.DefaultPolicy;
+        });
+
+        var app = builder.Build();
+
+        app.MapDefaultEndpoints();
+
+        app.UseDefaultFiles();
+        app.MapStaticAssets();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.MapFallbackToFile("/index.html");
+
+        app.Run();
+    }
+}
