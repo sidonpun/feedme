397542951841c1472f38559b20b3814d6d7fa8c6
maksim <Moonl3rr@gmail.com>
2025-06-29 04:38:32 +0400

    29.06.2025 4:38

--- feedme.client/src/app/components/CatalogTableComponent/catalog-table.component.ts
+++ feedme.client/src/app/components/CatalogTableComponent/catalog-table.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TableControlsComponent } from '../table-controls/table-controls.component';
 
 @Component({
@@ -18,13 +17,26 @@ export class CatalogTableComponent {
   currentPage: number = 1;
 
   get filteredData(): any[] {
+    const q = this.searchQuery.toLowerCase();
     return this.data.filter(item =>
-      item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      item.name.toLowerCase().includes(q)
     );
   }
 
   get paginatedData(): any[] {
     const start = (this.currentPage - 1) * this.rowsPerPage;
     return this.filteredData.slice(start, start + this.rowsPerPage);
+  }
+
+  nextPage() {
+    if ((this.currentPage * this.rowsPerPage) < this.filteredData.length) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
   }
 }
--- feedme.client/src/app/components/StockTableComponent/stock-table.component.ts
+++ feedme.client/src/app/components/StockTableComponent/stock-table.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TableControlsComponent } from '../table-controls/table-controls.component';
 
 @Component({
@@ -13,20 +12,32 @@ import { TableControlsComponent } from '../table-controls/table-controls.compone
 export class StockTableComponent {
   @Input() data: any[] = [];
   @Output() onSettingsClick = new EventEmitter<any>();
-  @Output() delete = new EventEmitter<any>();
 
   searchQuery: string = '';
   rowsPerPage: number = 10;
   currentPage: number = 1;
 
   get filteredData(): any[] {
+    const q = this.searchQuery.toLowerCase();
     return this.data.filter(item =>
-      item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      item.name.toLowerCase().includes(q)
     );
   }
 
   get paginatedData(): any[] {
     const start = (this.currentPage - 1) * this.rowsPerPage;
     return this.filteredData.slice(start, start + this.rowsPerPage);
+  }
+
+  nextPage() {
+    if ((this.currentPage * this.rowsPerPage) < this.filteredData.length) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
   }
 }
