995872e796d9fba52887bb3c063aa0587429b31d
sidonpun <sidonypunjek@hotmail.com>
2025-07-30 18:55:41 +0400

    Configure karma and update tests

--- feedme.client/src/app/app.component.ts
+++ feedme.client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ContentComponent } from './components/content/content.component';
@@ -8,12 +7,11 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    ContentComponent,
-    SidebarComponent,
-    RouterOutlet
-  ],
+    imports: [
+      HeaderComponent,
+      ContentComponent,
+      SidebarComponent
+    ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
