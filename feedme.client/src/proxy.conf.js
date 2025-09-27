97b95e28f03207d723ab9ee0475f7430a77e6b0f
sidonpun <sidonypunjek@hotmail.com>
2025-09-28 02:37:29 +0400

    Configure frontend proxy to target production API

--- feedme.client/src/proxy.conf.js
+++ feedme.client/src/proxy.conf.js
@@ -1,15 +1,22 @@
 const { env } = require('process');
 
-const target = env["services__feedme-server__https__0"] ?? 'https://localhost:7221';
+const DEFAULT_TARGET = 'http://185.251.90.40';
+const target =
+  env.FEEDME_API_BASE_URL ??
+  env["services__feedme-server__https__0"] ??
+  env["services__feedme-server__http__0"] ??
+  DEFAULT_TARGET;
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      '/api',
+      '/weatherforecast',
     ],
     target,
-    secure: false
-  }
-]
+    secure: false,
+    changeOrigin: true,
+  },
+];
 
 module.exports = PROXY_CONFIG;
